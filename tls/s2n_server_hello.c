aws/s2n-tls

Adding client side support for HelloRetryRequests

tls/s2n_server_hello.c
@@ -105,6 +105,14 @@ static int s2n_server_hello_parse(struct s2n_connection *conn)
     GUARD(s2n_stuffer_read_bytes(in, protocol_version, S2N_TLS_PROTOCOL_VERSION_LEN));
     GUARD(s2n_stuffer_read_bytes(in, conn->secure.server_random, S2N_TLS_RANDOM_DATA_LEN));
 
+    /* If the client receives a second HelloRetryRequest in the same connection, it MUST send an error. */
+    if (s2n_check_if_hrr_random(conn)) {
+        if (conn->handshake.handshake_type & HELLO_RETRY_REQUEST) {
+            S2N_ERROR(S2N_ERR_BAD_MESSAGE);
+        }
+        GUARD(s2n_set_hello_retry_handshake(conn));
+    }
+
     GUARD(s2n_stuffer_read_uint8(in, &session_id_len));
     S2N_ERROR_IF(session_id_len > S2N_TLS_SESSION_ID_MAX_LEN, S2N_ERR_BAD_MESSAGE);
     GUARD(s2n_stuffer_read_bytes(in, session_id, session_id_len));

tls/s2n_tls13.h
@@ -26,6 +26,7 @@ int s2n_enable_tls13();
 int s2n_disable_tls13();
 bool s2n_is_valid_tls13_cipher(const uint8_t version[2]);
 
-bool s2n_is_hello_retry_required(struct s2n_connection *conn);
+bool s2n_is_hello_retry_message(struct s2n_connection *conn);
 bool s2n_is_hello_retry_handshake(struct s2n_connection *conn);
-int s2n_set_hello_retry_required(struct s2n_connection *conn);
+int s2n_set_hello_retry_handshake(struct s2n_connection *conn);
+bool s2n_check_if_hrr_random(struct s2n_connection *conn);

ttjsu-aws/s2n

tls/s2n_server_hello.c
@@ -105,6 +105,14 @@ static int s2n_server_hello_parse(struct s2n_connection *conn)
     GUARD(s2n_stuffer_read_bytes(in, protocol_version, S2N_TLS_PROTOCOL_VERSION_LEN));
     GUARD(s2n_stuffer_read_bytes(in, conn->secure.server_random, S2N_TLS_RANDOM_DATA_LEN));
 
+    /* If the client receives a second HelloRetryRequest in the same connection, it MUST send an error. */
+    if (s2n_is_hello_retry_valid(conn)) {
+        if (s2n_is_hello_retry_handshake(conn)) {
+            S2N_ERROR(S2N_ERR_BAD_MESSAGE);
+        }
+        GUARD(s2n_set_hello_retry_handshake(conn));
+    }
+
     GUARD(s2n_stuffer_read_uint8(in, &session_id_len));
     S2N_ERROR_IF(session_id_len > S2N_TLS_SESSION_ID_MAX_LEN, S2N_ERR_BAD_MESSAGE);
     GUARD(s2n_stuffer_read_bytes(in, session_id, session_id_len));
@@ -163,17 +171,6 @@ static int s2n_server_hello_parse(struct s2n_connection *conn)
     return 0;
 }
 
-/* Lets the client determine whether a HelloRetryRequest is valid */
-static bool s2n_server_hello_retry_is_valid(struct s2n_connection *conn)
-{
-    notnull_check(conn);
-
-    bool has_versions_ext = conn->server_protocol_version >= S2N_TLS13;
-    bool has_correct_random = (memcmp(hello_retry_req_random, conn->secure.server_random, S2N_TLS_RANDOM_DATA_LEN) == 0);
-
-    return has_versions_ext && has_correct_random && conn->client_protocol_version == S2N_TLS13;
-}
-
 int s2n_server_hello_recv(struct s2n_connection *conn)
 {
     notnull_check(conn);
@@ -183,7 +180,7 @@ int s2n_server_hello_recv(struct s2n_connection *conn)
 
     /* If this is a HelloRetryRequest, we don't process the ServerHello.
      * Instead we proceed with retry logic. */
-    if (s2n_server_hello_retry_is_valid(conn)) {
+    if (s2n_is_hello_retry_valid(conn)) {
         GUARD(s2n_server_hello_retry_recv(conn));
         return 0;
     }

tls/s2n_tls13.h
@@ -26,6 +26,7 @@ int s2n_enable_tls13();
 int s2n_disable_tls13();
 bool s2n_is_valid_tls13_cipher(const uint8_t version[2]);
 
-bool s2n_is_hello_retry_required(struct s2n_connection *conn);
+bool s2n_is_hello_retry_message(struct s2n_connection *conn);
 bool s2n_is_hello_retry_handshake(struct s2n_connection *conn);
-int s2n_set_hello_retry_required(struct s2n_connection *conn);
+int s2n_set_hello_retry_handshake(struct s2n_connection *conn);
+bool s2n_is_hello_retry_valid(struct s2n_connection *conn);